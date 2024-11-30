7b1724f15ddeab036a5f07d6d72e8e58ff1a3758
tanishqsujan <tanishq.kothamasu@gmail.com>
2024-11-30 11:29:12 +0400

    hw

--- vehi.java
+++ vehi.java
@@ -46,6 +46,51 @@ class car implements vehicle{
         System.out.println("Speed" +speed+ "Gear" +gear);
     }
 }
+class truck implements vehicle{
+    int speed;
+    int gear;
+    @Override
+    public void changegear(int newgear){
+        gear= newgear;
+    }
+    
+    @Override
+    public void speedup(int increment){
+        speed += increment;
+    }
+    
+    @Override
+    public void applybrakes(int decrement){
+        speed -= decrement;
+    }
+    public void printstates() {
+        System.out.println("Speed" + speed + "Gear" + gear);
+    }
+}
+
+class bike implements vehicle {
+    int speed;
+    int gear;
+
+    @Override
+    public void changegear(int newgear) {
+        gear = newgear;
+    }
+
+    @Override
+    public void speedup(int increment) {
+        speed += increment;
+    }
+
+    @Override
+    public void applybrakes(int decrement) {
+        speed -= decrement;
+    }
+
+    public void printstates() {
+        System.out.println("Speed" + speed + "Gear" + gear);
+    }
+}
 class vehi {
     public static void main(String args[]){
         bicycle b= new bicycle();
@@ -61,5 +106,19 @@ class vehi {
         c.applybrakes(3);
         System.out.println("Car present states");
         c.printstates();
+
+        truck t= new truck();
+        t.changegear(6);
+        c.speedup(4);
+        c.applybrakes(5);
+        System.out.println("Truck present states");
+        t.printstates();
+
+        bike k = new bike();
+        k.changegear(4);
+        k.speedup(3);
+        k.applybrakes(1);
+        System.out.println("Bike present states");
+        k.printstates();
     }
 }
